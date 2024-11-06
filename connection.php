611efda2dd7f25385eabdfd1cd8cd9da77e0c8e1
Amir Hasan Zaizakarani <43024238+aihara44@users.noreply.github.com>
2024-11-06 20:16:41 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "breyersg") or die ("Cannot connect to database");
+$con = mysqli_connect("aquademo99-server.mysql.database.azure.com", "wjodcsldbe", "gu$BgQ77N0bAVOeM", "aquademo99-server") or die ("Cannot connect to database");
 function mres ($con,$data){
     return mysqli_real_escape_string($con,rtrim(ltrim($data)));
 }
